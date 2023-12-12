83b281f7c677841862e608d436deb5aabeb309ed
tejasjagdale <tejasjagdale2003@gmail.com>
2023-12-13 04:52:01 +0530

    testing commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the FTCS Application
+          This is the FTCS Application again testing
         </p>
         <a
           className="App-link"
