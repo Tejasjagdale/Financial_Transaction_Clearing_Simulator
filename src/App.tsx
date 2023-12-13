dcea08ce24a60ab488973384bdf6f4a59c19b8d0
tejasjagdale <tejasjagdale2003@gmail.com>
2023-12-13 05:51:26 +0530

    login page setuped

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider } from "@mui/material";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Login from "./Pages/Login";
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0072E5", // Set your primary color here
+      dark: "#9DC2CC",
+      light: "#B1DEE0",
+    },
+    secondary: {
+      main: "#989898", // Set your secondary color here
+      light: "#00bcd4",
+      dark: "#CFCFCF",
+    },
+    info: {
+      main: "#FFFFFF",
+    },
+    success: {
+      main: "#2E7D32",
+    },
+  },
+});
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the FTCS Application again testing
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Login />} />
+            </Routes>
+          </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 }
 
 export default App;
+//
